ae61acf8c25dc45a59e06d96638d2ce766077cdc
Dario L Garcia <dario.garcia@10pines.com>
2019-06-13 00:37:26 -0300

    Ejemplo funcionando como TODO

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,21 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{title}}:</h1>
+
+    <input type="text" class="nes-input" placeholder="Add todo…" v-on:keyup.enter="addTodo">
+
+    <ul>
+      <li v-for="todo in todos" v-bind:key="todo.key" class="flex">
+        {{todo.text}}
+        <div class="space"></div>
+        <button class="nes-btn is-error padding" v-on:click="removeTodo(todo.key)">X</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
+  },
+
+  data() {
+    return {
+      title: 'TODOS',
+      todos: [
+        {text: 'todo 1', done: false, key: Date.now()},
+        {text: 'todo 2', done: false, key: Date.now() + 1}
+      ]
+    }
+  },
+  methods: {
+    addTodo(event) {
+      const text = event.target.value
+      this.todos.push({text, done: false, key: Date.now()})
+      event.target.value = ''
+    },
+    removeTodo(key) {
+      this.todos = this.todos.filter(todo => todo.key !== key)
+    }
   }
 }
 </script>
 
+<style ref="https://fonts.googleapis.com/css?family=Press+Start+2P" />
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
